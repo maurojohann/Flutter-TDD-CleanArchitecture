f6ed154eea66b4a7e37a8fe070786a4b3e329337
Mauro Cezar Johann <mauro.johann@gmail.com>
2021-06-15 20:59:49 -0300

    ensure HttpAdapter throws outher error 401,404,403

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -93,6 +93,27 @@ void main() {
 
       expect(future, throwsA(HttpError.badRequest));
     });
+    test('Should return UnauthorizeError if post returns 401', () async {
+      mockResponse(401);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+    test('Should return Forbidden if post returns 403', () async {
+      mockResponse(403);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.forbidden));
+    });
+    test('Should return NotFound if post returns 404', () async {
+      mockResponse(404);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.notFound));
+    });
     test('Should return ServerError if post returns 500', () async {
       mockResponse(500);
 
