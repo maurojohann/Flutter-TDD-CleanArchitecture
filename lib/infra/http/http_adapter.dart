1bc557b15badda395d26c4a14ed479f6ad2f990c
Mauro Cezar Johann <mauro.johann@gmail.com>
2021-06-15 19:08:16 -0300

    move duplicated code to a private method

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -4,22 +4,24 @@ import 'package:http/http.dart';
 import 'package:meta/meta.dart';
 import 'package:myapp/data/http/http.dart';
 
-class HttpAdapter implements HttpClients{
+class HttpAdapter implements HttpClients {
   final Client client;
 
   HttpAdapter(this.client);
 
-  Future<Map> request({
-    @required String url,
-    @required String method,
-    Map body
-  }) async {
+  Future<Map> request(
+      {@required String url, @required String method, Map body}) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
-    final response = await client.post(Uri.parse(url), headers: headers, body: jsonBody);
+    final response =
+        await client.post(Uri.parse(url), headers: headers, body: jsonBody);
+    return _handdleResponse(response);
+  }
+
+  Map _handdleResponse(Response response) {
     if (response.statusCode == 200) {
       return response.body.isEmpty ? null : jsonDecode(response.body);
     } else {
