99a4c62345502383bee9c26a1c7c400494efb2ed
Mauro Cezar Johann <mauro.johann@gmail.com>
2021-06-15 21:09:22 -0300

    ensure HttpAdapter throws ServerError if invalid method is provider

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -16,8 +16,11 @@ class HttpAdapter implements HttpClients {
       'accept': 'application/json'
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
-    final response =
-        await client.post(Uri.parse(url), headers: headers, body: jsonBody);
+    var response = Response('', 500);
+    if (method == 'post') {
+      response =
+          await client.post(Uri.parse(url), headers: headers, body: jsonBody);
+    }
     return _handdleResponse(response);
   }
 
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -19,6 +21,13 @@ void main() {
     url = faker.internet.httpUrl();
   });
 
+  group('shared', () {
+    test('Should throw ServerError if invalid method is provided', () async {
+      final future = sut.request(url: url, method: 'invalid_method');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+  });
   group('post', () {
     PostExpectation mockRequest() => when(
         client.post(any, body: anyNamed('body'), headers: anyNamed('headers')));
